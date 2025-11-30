1b1051242001bd5e34647a8f8dfc67ff918d6534
Giustino Sulpizio <giustino.i.sulpizio@gmail.com>
2025-11-30 01:06:30 +0100

    remove: Redundant training parameters

--- smiles_transformer/main.py
+++ smiles_transformer/main.py
@@ -450,8 +450,6 @@ def train(
         save_dataset_path=params["dataset_settings"]["save_dataset_path"],
         load_dataset_path=params["dataset_settings"]["load_dataset_path"],
         params=params,
-        weight_decay=params["training_settings"].get("weight_decay", 0.0),
-        max_grad_norm=params["training_settings"].get("max_grad_norm", 1.0),
         label_smoothing=params["training_settings"].get("label_smoothing", 0.0),
         freeze_encoder_steps=params["training_settings"].get("freeze_encoder_steps", None),
         gradual_unfreezing=params["training_settings"].get("gradual_unfreezing", False),
